9bee19201a7e08461ccd041785cd33686e95aaf0
Keith <keith@automara.com>
2025-11-08 10:12:28 -0700

    chore: place mastra config at repo root

---
+++ dist/mastra.config.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    agents: {
+        demoAgent: () => Promise<typeof import("./src/mastra/index.js")>;
+    };
+};
+export default _default;
+//# sourceMappingURL=mastra.config.d.ts.map
